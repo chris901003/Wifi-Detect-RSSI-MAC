fc7fde0263979adf13cafe6e8bae646688e35a64
chris901003 <a0987999103@gmail.com>
2023-12-07 22:05:23 +0800

    Initial Commit

---
+++ Wifi-Detect-RSSI-Mac/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Wifi-Detect-RSSI-Mac
+//
+//  Created by 黃弘諺 on 2023/12/7.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Wifi-Detect-RSSI-Mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Wifi-Detect-RSSI-Mac
+//
+//  Created by 黃弘諺 on 2023/12/7.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
