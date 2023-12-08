57f1b1b002a1d48dae65d22e4e0148b53af487c8
chris901003 <a0987999103@gmail.com>
2023-12-08 23:13:20 +0800

    feat: init from network

--- Wifi-Detect-RSSI-Mac/Data/WifiData.swift
+++ Wifi-Detect-RSSI-Mac/Data/WifiData.swift
@@ -5,10 +5,20 @@
 //  Created by 黃弘諺 on 2023/12/8.
 //
 
+import CoreWLAN
+
 struct WifiData {
     let name: String
     let rssi: Int
     let type: WifiType
+
+    static func fromNetwork(network: CWNetwork) -> WifiData {
+        let name = network.ssid ?? "未知"
+        let rssi = network.rssiValue
+        let channel = network.wlanChannel?.channelBand
+        let type: WifiType = channel == .band2GHz ? .G2_4 : .G5
+        return WifiData(name: name, rssi: rssi, type: type)
+    }
 }
 
 enum WifiType {
